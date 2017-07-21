f2dce69a18b8ade91bba2eb4284348de00e5791b
unadlib <unadlib@gmail.com>
2017-07-22 02:21:39 +0800

    Alpha

---
+++ example/cssTree.js
@@ -0,0 +1,60 @@
+/**
+ * Author   : unadlib
+ * Date     : 2017/7/19
+ * Time     : 上午12:30
+ * Project [ react-native-css-tree ] Coded on WebStorm.
+ */
+
+const inheritRegular = /^\_/;
+const variableRegular = /\$\./;
+
+export function createCSS(init = {}) {
+    return function (...processes) {
+        return function (styles) {
+            return getTree(styles, init, processes);
+        };
+    }
+}
+
+function isPlainObject(arg) {
+    return toString.call(arg) === "[object Object]";
+}
+
+function parseStyle(style = {}, init = {}, processes = [], parent = {}) {
+    Object.keys(style).map(i => {
+        let isSubNode = isPlainObject(style[i]);
+        let clone = JSON.parse(JSON.stringify(style[i]));
+        if (isSubNode) {
+            let newStyle = inheritRegular.test(i) ? Object.assign(style[i], css(style), clone) : style[i];
+            processes.map(fn => {
+                newStyle = fn(i, style, newStyle);
+            });
+            parseStyle(newStyle, init, processes, style);
+        } else {
+            if (variableRegular.test(style[i])) {
+                style[i] = new Function("$", `return ${style[i]}`)({
+                    ...init,
+                    ...css(parent)
+                });
+            }
+        }
+    });
+}
+
+function getTree(styles = {}, init = {}, processes = []) {
+    let $styles = {};
+    Object.keys(styles).map(i => {
+        let root = styles[i];
+        parseStyle(root, init, processes, init);
+        $styles[i] = root;
+    });
+    return $styles;
+}
+
+export function css(style = {}) {
+    let $style = {};
+    Object.keys(style).map(name => {
+        if (!isPlainObject(style[name])) $style[name] = style[name];
+    });
+    return $style;
+};
--- example/index.ios.js
+++ example/index.ios.js
@@ -4,50 +4,60 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
 } from 'react-native';
+import {css, createCSS} from 'react-native-css-tree';
 
 export default class example extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={css(styles.container)}>
+                <Text style={css(styles.container.welcome)}>
+                    Welcome to React Native!
+                </Text>
+                <Text style={css(styles.container.instructions)}>
+                    To get started, edit index.ios.js
+                </Text>
+                <Text style={css(styles.container.instructions)}>
+                    Press Cmd+R to reload,{'\n'}
+                    Cmd+D or shake for dev menu
+                </Text>
+            </View>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
+const style = {
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: "$.mainColor",
+        welcome: {
+            fontSize: "$.fontSize",
+            textAlign: '$.alignItems',
+            margin: "$.grid",
+        },
+        instructions: {
+            textAlign: '$.alignItems',
+            color: '$.textColor',
+        },
+    },
+};
+
+const styles = createCSS({
+    mainColor: '#00d1ff',
+    textColor: "#333333",
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+    grid: 10,
+})(function (key, parent, sub) {
+    sub.marginBottom = 5;
+    return sub
+})(style);
 
 AppRegistry.registerComponent('example', () => example);
